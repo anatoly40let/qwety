52a5a51cfb29a07d29b071a3312f90e0634246fc
anatoly40let <naprodajyakkaynt@gmail.com>
2026-03-13 11:24:51 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+Console.Title = "Моё первое приложение";
+    string name = Console.ReadLine();
+Console.WriteLine()$"\nРад знакомству, {name}!");
+    Console.WriteLine()$"Сегодня на календаре: {DateTime.Now:dd.MM.yyyy}");
+    Console.WriteLine()"Нажми любую клавишу, чтобы выйти...");
+    Console.ReadKey();
